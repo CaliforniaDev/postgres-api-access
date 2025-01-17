158f13b2d588ed93839b2dc82f558de0abffc9b1
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-17 00:24:47 -0800

    Add users route with CRUD operations

---
+++ src/routes/users.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/users", async (req, res) => {});
+
+router.get("/users/:id", async (req, res) => {});
+
+router.post("/users", async (req, res) => {});
+
+router.put("/users/:id", async (req, res) => {});
+
+router.delete("/users/:id", async (req, res) => { });
+
+export default router;
