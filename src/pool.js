dbdb1fadc4eadb6b8df63a9c37bd2d0087549023
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-17 00:23:55 -0800

    Add PostgreSQL connection pool implementation

---
+++ src/pool.js
@@ -0,0 +1,20 @@
+import pg from "pg";
+
+class Pool {
+  _pool = null;
+  connect(options) {
+    this._pool = new pg.Pool(options);
+    return this._pool.query("SELECT 1 + 1");
+  }
+  close() {
+    return this._pool.end();
+  }
+  /**
+   * ! Security Risk
+   */
+  query(sql) {
+    return this._pool.query(sql);
+  }
+}
+
+export default new Pool();
