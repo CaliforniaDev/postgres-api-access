e67cd4e92f3c6f9621dddefb35533cb8302cb6e3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-17 15:04:54 -0800

    Refactor users route: replace old users.js with users.mjs and integrate UserRepo for database queries

--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -1,7 +1,15 @@
 import express from "express";
+import UserRepo from "../repos/user-repo.mjs";
 const router = express.Router();
 
-router.get("/users", async (req, res) => {});
+router.get("/users", async (req, res) => {
+  // Run a query to get all
+  const users = await UserRepo.find();
+  // Send the result back to the client
+  // Who made this request?
+  res.send(users);
+  
+});
 
 router.get("/users/:id", async (req, res) => {});
 
