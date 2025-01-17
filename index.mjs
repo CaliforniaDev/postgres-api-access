633ff0327cadecd1ab4ad6e985fcb5babd81d3c4
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-17 15:12:24 -0800

    Refactor database connection options in index.mjs for improved readability

--- index.mjs
+++ index.mjs
@@ -2,15 +2,16 @@ import createApp from "./src/app.mjs";
 import pool from "./src/pool.mjs";
 
 const app = createApp();
+const options = {
+  host: "localhost",
+  port: 5432,
+  database: "social_api_access",
+  user: "leodaniels",
+  password: "",
+};
 
 pool
-  .connect({
-    host: "localhost",
-    port: 5432,
-    database: "social_api_access",
-    user: "leodaniels",
-    password: "",
-  })
+  .connect(options)
   .then(() => {
     app.listen(3005, () => {
       console.log("Listening on port 3005");
