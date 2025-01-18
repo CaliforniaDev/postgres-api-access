a6dce77550909a42de9e6173c9d92020ee5b2c85
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-17 16:29:32 -0800

    Refactor user repository to use single quotes for string literals and transform database rows to camelCase format

--- src/repos/user-repo.mjs
+++ src/repos/user-repo.mjs
@@ -4,12 +4,30 @@
  * It should be able to find, insert, update, and delete users in the database.
  */
 
-import pool from "../pool.mjs";
+import pool from '../pool.mjs';
 
 class UserRepo {
   static async find() {
-    const { rows } = await pool.query("SELECT * FROM users");
-    return rows;
+    const { rows } = await pool.query('SELECT * FROM users');
+    const parsedRows = rows.map((row) => {
+      return Object.entries(row).reduce((acc, [key, value]) => {
+        const camelCase = key.replace(/([-_][a-z])/gi, ($1) =>
+          $1.toUpperCase().replace('_', ''),
+        );
+        acc[camelCase] = value;
+        return acc;
+      }, {});
+
+      // const replaced = {};
+      // for (let key in row) {
+      //   const camelCase = key.replace(/([-_][a-z])/gi, ($1) =>
+      //     $1.toUpperCase().replace('_', ''),
+      //   );
+      //   replaced[camelCase] = row[key];
+      // }
+      // return replaced;
+    });
+    return parsedRows;
   }
 
   static async findById() {}
