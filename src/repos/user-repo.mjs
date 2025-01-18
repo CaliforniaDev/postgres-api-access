9dfd9ab2c2642b3511d26252fe25455823299bb0
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-17 16:29:55 -0800

    Remove commented-out code for transforming database rows to camelCase in user repository

--- src/repos/user-repo.mjs
+++ src/repos/user-repo.mjs
@@ -17,15 +17,6 @@ class UserRepo {
         acc[camelCase] = value;
         return acc;
       }, {});
-
-      // const replaced = {};
-      // for (let key in row) {
-      //   const camelCase = key.replace(/([-_][a-z])/gi, ($1) =>
-      //     $1.toUpperCase().replace('_', ''),
-      //   );
-      //   replaced[camelCase] = row[key];
-      // }
-      // return replaced;
     });
     return parsedRows;
   }
