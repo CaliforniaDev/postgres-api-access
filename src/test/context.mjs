c9f07f04ae6de99bd619d36ca6a53c5d2815455b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-21 06:56:17 -0800

    feat: Introduce Context class for isolated database role and schema management in tests

---
+++ src/test/context.mjs
@@ -0,0 +1,65 @@
+import pool from '../pool.mjs';
+import format from 'pg-format';
+import migrate from 'node-pg-migrate';
+import { randomBytes } from 'crypto';
+
+class Context {
+  static async build() {
+    // Randomly generate a new database name to ensure isolation
+    const roleName = 'a' + randomBytes(4).toString('hex');
+
+    // Connect to the "socialnetwork-test" database
+    await pool.connect({
+      host: 'localhost',
+      port: 5432,
+      database: 'socialnetwork-test',
+      user: 'leodaniels',
+      password: '',
+    });
+
+    //Create a new role
+    // Using pg-format to prevent SQL injection I% means identifier, L% means literal
+    await pool.query(
+      format('CREATE ROLE %I WITH LOGIN PASSWORD %L', roleName, roleName),
+    );
+
+    // Create a schema with the same name
+    await pool.query(
+      format('CREATE SCHEMA %I AUTHORIZATION %I', roleName, roleName),
+    );
+
+    // Disconnect from the database
+    await pool.close();
+
+    // Run our migrations in the new schema
+    await migrate({
+      schema: roleName,
+      direction: 'up',
+      log: () => {},
+      noLock: true, // Prevents the migration from hanging if there are other connections
+      dir: 'migrations',
+      databaseUrl: {
+        host: 'localhost',
+        port: 5432,
+        database: 'socialnetwork-test',
+        user: roleName,
+        password: roleName,
+      },
+    });
+
+    // Connect to database as the new role
+    await pool.connect({
+      host: 'localhost',
+      port: 5432,
+      database: 'socialnetwork-test',
+      user: roleName,
+      password: roleName,
+    });
+    return new Context(roleName);
+  }
+  constructor(roleName) {
+    this.roleName = roleName;
+  }
+}
+
+export default Context;
--- src/test/routes/users.test.mjs
+++ src/test/routes/users.test.mjs
@@ -3,61 +3,13 @@ import buildApp from '../../app.mjs';
 import UserRepo from '../../repos/user-repo.mjs';
 
 import pool from '../../pool.mjs';
-import { randomBytes } from 'crypto';
-import migrate from 'node-pg-migrate';
-import format from 'pg-format';
+import Context from '../context.mjs';
+// import { randomBytes } from 'crypto';
+// import migrate from 'node-pg-migrate';
+// import format from 'pg-format';
 
 beforeAll(async () => {
-  // Randomly generate a new database name to ensure isolation
-  const roleName = 'a' + randomBytes(4).toString('hex');
-
-  // Connect to the "socialnetwork-test" database
-  await pool.connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'socialnetwork-test',
-    user: 'leodaniels',
-    password: '',
-  });
-
-  //Create a new role
-  // Using pg-format to prevent SQL injection I% means identifier, L% means literal
-  await pool.query(
-    format('CREATE ROLE %I WITH LOGIN PASSWORD %L', roleName, roleName),
-  );
-
-  // Create a schema with the same name
-  await pool.query(
-    format('CREATE SCHEMA %I AUTHORIZATION %I', roleName, roleName),
-  );
-
-  // Disconnect from the database
-  await pool.close();
-
-  // Run our migrations in the new schema
-  await migrate({
-    schema: roleName,
-    direction: 'up',
-    log: () => {},
-    noLock: true, // Prevents the migration from hanging if there are other connections
-    dir: 'migrations',
-    databaseUrl: {
-      host: 'localhost',
-      port: 5432,
-      database: 'socialnetwork-test',
-      user: roleName,
-      password: roleName,
-    },
-  });
-
-  // Connect to database as the new role
-  await pool.connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'socialnetwork-test',
-    user: roleName,
-    password: roleName,
-  });
+  const context = await Context.build();
 });
 
 afterAll(() => {
