9f7f2a066a9766b9f43fa1852da5ce4da25e16e2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-22 04:57:52 -0800

    feat: Add reset and close methods to Context class for database cleanup

--- src/test/context.mjs
+++ src/test/context.mjs
@@ -3,19 +3,21 @@ import format from 'pg-format';
 import migrate from 'node-pg-migrate';
 import { randomBytes } from 'crypto';
 
+const DEFAULT_OPTIONS = {
+  host: 'localhost',
+  port: 5432,
+  database: 'socialnetwork-test',
+  user: 'leodaniels',
+  password: '',
+};
+
 class Context {
   static async build() {
     // Randomly generate a new database name to ensure isolation
     const roleName = 'a' + randomBytes(4).toString('hex');
 
     // Connect to the "socialnetwork-test" database
-    await pool.connect({
-      host: 'localhost',
-      port: 5432,
-      database: 'socialnetwork-test',
-      user: 'leodaniels',
-      password: '',
-    });
+    await pool.connect(DEFAULT_OPTIONS);
 
     //Create a new role
     // Using pg-format to prevent SQL injection I% means identifier, L% means literal
@@ -59,6 +61,22 @@ class Context {
   }
   constructor(roleName) {
     this.roleName = roleName;
+  }
+  async reset() {
+    return pool.query(`
+      DELETE FROM users;
+    `);
+  }
+  async close() {
+    // Disconnect from the database
+    await pool.close();
+    // Reconnect as our root user
+    await pool.connect(DEFAULT_OPTIONS);
+    // Delete the role and schema
+    await pool.query(format('DROP SCHEMA %I CASCADE', this.roleName));
+    await pool.query(format('DROP ROLE %I', this.roleName));
+    // Disconnect from the database
+    await pool.close();
   }
 }
 
